242b9790eea962bfcf35bf713eb00d9d3f2cb1fc
xinxin5080 <xinxin5080@163.com>
2018-12-16 15:38:34 +0800

    初始化

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [{
+    path: '/',
+    name: 'login',
+    // 按需加载，作用是吧login当成文件名的js
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+  }]
+})
