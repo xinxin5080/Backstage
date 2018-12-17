3a929c11f6ad5d0d9fe20cf2d1fe34f3d4e28b90
xinxin5080 <xinxin5080@163.com>
2018-12-17 14:26:15 +0800

    全局路由守卫

--- src/main.js
+++ src/main.js
@@ -9,7 +9,23 @@ import 'element-ui/lib/theme-chalk/index.css'
 // 使用
 Vue.use(ElementUI)
 Vue.config.productionTip = false
-
+// 路由守卫
+router.beforeEach((to, from, next) => {
+// 获取token
+  let mytoken = localStorage.getItem('mytoken')
+  if (mytoken) {
+    // 有mytoken就可以执行下步
+    next()
+  } else {
+    // 没有token时,不是首页就跳回首页
+    if (to.name !== 'login') {
+      next('/')
+    } else {
+      // 是首页就下一步
+      next()
+    }
+  }
+})
 new Vue({
   router,
   store,
